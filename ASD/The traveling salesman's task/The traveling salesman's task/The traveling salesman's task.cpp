d2c8cfd073a1f61d8831477d3fc3dacd6e6efa0d
Dubrovskirill <zet10set11@gmail.com>
2023-09-22 17:35:55 +0700

    Добавление отчёта

--- ASD/The traveling salesman's task/The traveling salesman's task/The traveling salesman's task.cpp
+++ ASD/The traveling salesman's task/The traveling salesman's task/The traveling salesman's task.cpp
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<iostream>
 #include<windows.h>
+#include <chrono>
 
 void OutputMatrD(int** matr, int m, int n)
 {
@@ -60,7 +61,7 @@ void CopyMas(int* a, int* b, int n)
 	for (int i = 0; i < n; i++) b[i] = a[i];
 }
 
-int Dijkstra(int const count_sity,int start_sity, int**Matr_Way_Weight,int *Min_Way)
+void Dijkstra(int const count_sity,int start_sity, int**Matr_Way_Weight,int *Min_Way,int *Max_Way, int &min_way_weight, int &max_way_weight)
 {
 	int *Way =new int[count_sity + 1];
 	Way[0] = start_sity;
@@ -73,23 +74,27 @@ int Dijkstra(int const count_sity,int start_sity, int**Matr_Way_Weight,int *Min_
 			i++;
 		}
 	//OutMasPtr(Way, count_sity + 1);//вывод начального пути
-	int MIN_Way_weight = MinWay(Way, Matr_Way_Weight, count_sity - 1);
-	//cout << " " << MIN_Way_weight << endl;//вес
+	min_way_weight=max_way_weight = MinWay(Way, Matr_Way_Weight, count_sity - 1);
+	//std::cout << " " << min_way_weight << std::endl;//вес
 	//перестановки
 	CopyMas(Way, Min_Way, count_sity + 1);
 	while (Permutation(count_sity, Way))
 	{
 		//OutMasPtr(Way, count_sity + 1);//вывод перестановки
-		int m_Way_weight = MinWay(Way, Matr_Way_Weight, count_sity - 1);
-		//cout << " " << m_Way_weight << endl;//вывод длины пути
-		if (m_Way_weight < MIN_Way_weight)
+		int way_weight = MinWay(Way, Matr_Way_Weight, count_sity - 1);
+		//std::cout << " " << way_weight << std::endl;//вывод длины пути
+		if (way_weight < min_way_weight)
 		{
-			MIN_Way_weight = m_Way_weight;
+			min_way_weight = way_weight;
 			CopyMas(Way, Min_Way, count_sity + 1);
+		}
+		if (way_weight > max_way_weight)
+		{
+			max_way_weight = way_weight;
+			CopyMas(Way, Max_Way, count_sity + 1);
 		}
 	}
 	delete[]Way;
-	return MIN_Way_weight;
 }
 
 int Find_cols(int* a, int n, int D)
@@ -99,14 +104,10 @@ int Find_cols(int* a, int n, int D)
 	return -1;
 }
 
-bool Сycle(int r, int c, int* arr, int n)//проверка на цикл
+bool Cycle(int r, int c, int* arr, int n)//проверка на цикл
 {
 	int i,j;
-	/*for ( i = 0; i < n; i += 2)
-		for ( j = 1; j < n; j += 2)
-			if (arr[i] == c && arr[j] == r) return false;
-	return true;*/
-	for (i = 0, j = 0; i < n; i += 2, j += 2)
+	for (i = 0, j = 1; i < n; i += 2, j += 2)
 		if (arr[i] == c && arr[j] == r) return false;
 	i = Find_cols(arr, n, r);
 	while (i != -1)
@@ -141,7 +142,7 @@ int Heuristics_1(int const count_sity, int start_sity, int** Matr_Way_Weight, in
 			for (c = 0; c < count_sity; c++)
 				if (r != c && Exceptions(way_data, n, r, c)  && Matr_Way_Weight[r][c] < min_weight)
 				{
-					if (i != count_sity-1)  flag = Сycle(r, c, way_data, n);
+					if (i != count_sity-1)  flag = Cycle(r, c, way_data, n);
 					if (flag)
 					{
 						row = r; coll = c;
@@ -150,19 +151,9 @@ int Heuristics_1(int const count_sity, int start_sity, int** Matr_Way_Weight, in
 				}
 		way_data[n] = row; way_data[n + 1] = coll;
 		n += 2;
-		OutMasPtr(way_data, count_sity*2);
-		std::cout << " " << min_weight << std::endl;
+		//OutMasPtr(way_data, count_sity*2);
+		//std::cout << " " << min_weight << std::endl;
 	}
-	/*min_weight = 100;
-	for (r = 0; r < count_sity; r++)
-		for (c = 0; c < count_sity; c++)
-			if (r != c && Exceptions(way_data, n, r, c) && Matr_Way_Weight[r][c] < min_weight)
-			{
-				row = r; coll = c;
-				min_weight = Matr_Way_Weight[r][c];
-			}
-	way_data[n] = row; way_data[n + 1] = coll;
-	n += 2;*/
 	/*OutMasPtr(way_data, count_sity * 2);
 	std::cout << " " << min_weight << std::endl;*/
 	int k = 0,i,j;
@@ -179,6 +170,19 @@ int Heuristics_1(int const count_sity, int start_sity, int** Matr_Way_Weight, in
 	delete[]way_data;
 	return weight;
 }
+
+float Percentag_Quality(int way_weight_h, int min_way_weight, int max_way_weight)
+{
+	float p; float a = way_weight_h - min_way_weight; float b = max_way_weight - min_way_weight;
+	float d = a / b;
+	return 100 - d * 100;
+}
+float Percentag_Speed(float time_h,float time)
+{
+	return  time / time_h * 100;
+
+}
+
 int main()
 {
 	using std::cout;
@@ -189,9 +193,9 @@ int main()
 	int** Matr_Way_Weight;
 	int count_sity;//задаем количество городов
 	int start_sity;//задаем начальный город
-	cout << "Введите количество городов: ";
+	cout << "Количество городов: ";
 	cin >> count_sity;
-	cout <<endl<< "Введите начальный город: ";
+	cout <<endl<< "Начальный город: ";
 	cin >> start_sity;
 	cout << endl;
 	int i, j;
@@ -205,24 +209,36 @@ int main()
 	cout << endl;
 	//задаем начальный путь
 	int *Min_Way=new int [count_sity + 1];
-	clock_t start = clock();
-	int MIN_way_weight=Dijkstra(count_sity, start_sity, Matr_Way_Weight,Min_Way);
-	clock_t end = clock();
-	float seconds = (float)(end - start) / CLOCKS_PER_SEC;
-	cout << "Кратчайший путь и его вес: ";
+	int *Max_Way = new int[count_sity + 1];
+	int min_way_weight;
+	int max_way_weight;
+
+	auto start = std::chrono::high_resolution_clock::now();
+	Dijkstra(count_sity, start_sity, Matr_Way_Weight,Min_Way,Max_Way,min_way_weight,max_way_weight);
+	auto end = std::chrono::high_resolution_clock::now();
+	float time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+	cout << "Наилучший путь и его вес: ";
 	OutMasPtr(Min_Way, count_sity + 1);
-	cout << " " << MIN_way_weight << endl<< seconds<<endl;
+	cout << " " << min_way_weight << endl;
+	cout << "Наихудший путь и его вес: ";
+	OutMasPtr(Max_Way, count_sity + 1);
+
+	cout << " " << max_way_weight << endl << "Время работы: " << time <<" наносекунд"<< endl;
 	delete[]Min_Way;
 	//эвристика
 	int* Min_Way_H = new int[count_sity+1];
-	start = clock();
-	int weight=Heuristics_1(count_sity, start_sity-1, Matr_Way_Weight, Min_Way_H);
-	end = clock();
-	seconds = (float)(end - start) / CLOCKS_PER_SEC;
-	cout << "Кратчайший путь и его вес: ";
-	cout << endl;
+	start = std::chrono::high_resolution_clock::now();
+	int way_weight_h=Heuristics_1(count_sity, start_sity-1, Matr_Way_Weight, Min_Way_H);
+	end = std::chrono::high_resolution_clock::now();
+	float time_h = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+	cout << "Эвристическое решение; путь и его вес: ";
 	OutMasPtr(Min_Way_H, count_sity + 1);
-	cout << " " << weight << endl << seconds << endl;
+	cout << " " << way_weight_h << endl <<"Время работы: " << time_h<<" наносекунд" << endl;
+	// отчет
+	float perc_q = Percentag_Quality(way_weight_h,min_way_weight,max_way_weight);
+	float perc_s = Percentag_Speed(time_h, time);
+	cout << "Процент качества: "<<perc_q<<"%"<<endl<< "Процент скорости: " << perc_s << "%" << endl;
+	
 	delete[]Min_Way_H;
 	for (int i = 0; i < count_sity; i++)
 		delete[]Matr_Way_Weight[i];
