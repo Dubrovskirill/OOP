7012337c08e4de5d340ee0fa0ea9366d180f4a16
Dubrovskirill <zet10set11@gmail.com>
2023-09-28 11:12:03 +0700

    Создание класса Array

---
+++ OOP/OOP_LAB_2/OOP_LAB_2/Array.h
@@ -0,0 +1,16 @@
+#pragma once
+class Array
+{
+public:
+	Array(const int size = 10, const int value = 0);
+	~Array();
+
+	void print();
+
+	int &operator[](const int index);
+
+private:
+	int* m_array = nullptr;
+	int m_size = 0;
+};
+
