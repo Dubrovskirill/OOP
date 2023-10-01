5419e0cb42650409d4fd52b6d3b8d7b822539712
Dubrovskirill <zet10set11@gmail.com>
2023-10-01 17:42:22 +0700

    Добавление поиска на промежутке

--- ASD/Boyer-Moore search algorithm/Boyer-Moore search algorithm/main.cpp
+++ ASD/Boyer-Moore search algorithm/Boyer-Moore search algorithm/main.cpp
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<iostream>
+#include<vector>
 
 void OutputArr(int* a, int n)
 {
@@ -19,18 +20,20 @@ std::string ShiftStringLeft(std::string s, int i)
 }
 
 
-int BMsearch(std::string text, std::string substring)
+int BMsearch(std::string text, std::string substring, int first=0, int l_text=-1)
 {
-	int const l_text = text.length();
-	int const l_substring = substring.length();
+	if(l_text<0)  l_text = text.length();
+	//else int const l_text = last;
+	int l_substring = substring.length();
 	int TAB[256];
-	int i,j, k;
-	for (i = 0; i < l_text; i++) TAB[text[i]] = l_substring;
+	int i,j, k=0;
+	for (i = first; i < l_text; i++) TAB[text[i]] = l_substring;
 	for (j = 0; j < l_substring - 1; j++) TAB[substring[j]] = l_substring - 1 - j;
 
-	for (i = l_substring - 1, j = l_substring - 1; i <= l_text; i = k + TAB[text[k]])
+	for (i = first+ l_substring - 1, j = l_substring - 1; i <= l_text; i = k + TAB[text[k]])
 	{
 		if (j < 0) return k+1;
+		if (k >= l_text) return -1;
 		for (k = i, j = l_substring - 1; j >= 0 && text[k] == substring[j]; )
 		{
 			k--;
@@ -41,23 +44,37 @@ int BMsearch(std::string text, std::string substring)
 	if (i >= l_text && j < 0) return k + 1;
 	else return -1;
 }
-
-int BMsearchAll(std::string text, std::string substring, int *Occurrence)
+std::vector<int> BMsearchAllRange(std::string text, std::string substring,int first, int last)
 {
-	std::string t = text;
+	std::vector<int> Occurrence;
+	int i = 0;
+	int occ = BMsearch(text, substring,first,last);
+	if (occ == -1) return Occurrence;
+	while (occ != -1)
+	{
+		Occurrence.push_back(occ);
+		first = occ+1;
+		occ = BMsearch(text, substring, first, last);
+	}
+	return Occurrence;
+}
+
+std::vector<int> BMsearchAll(std::string text, std::string substring)
+{
+	/*std::string t = text;
+	std::vector<int> Occurrence;
 	int i = 0;
 	int occ = BMsearch(t,substring);
-	if (occ == -1) return -1;
+	if (occ == -1) return Occurrence;
 	int sourse_i = 0;
 	while (occ != -1)
 	{
-		Occurrence[i] = occ+ sourse_i;
-		i++;
+		Occurrence.push_back(occ+sourse_i);
 		t = ShiftStringLeft(t, occ+1);
 		sourse_i += occ+1;
 		occ = BMsearch(t, substring);
-	}
-	return i;
+	}*/
+	return BMsearchAllRange(text, substring, 0, text.length());
 }
 
 int main()
@@ -70,17 +87,25 @@ int main()
 	string substring = "tor";
 	cout <<"STRING: "<< text << endl << "It's length: " <<text.length() << endl << "SUBSTRING: " << substring << endl;
 	int first_occurrence = BMsearch(text, substring);
-	if (first_occurrence == -1) cout << "There is no such substring in the string";
+	if (first_occurrence == -1) cout << "There is no such substring in the string" << endl;
 	else cout << "Index of the first occurrence: " << first_occurrence<<endl;
-	//cout << ShiftStringLeft(text, 50);
-	int *Occurrence=new int[text.length()];
-	int size_arr = BMsearchAll(text, substring, Occurrence);
-	if (size_arr == -1) cout << "There is no such substring in the string";
-	else cout << "Indexes of each occurrence: ";
-	OutputArr(Occurrence, size_arr);
+	
+	std::vector<int> Occurrence = BMsearchAll(text, substring);
+	if (Occurrence.size()==0) cout << "There is no such substring in the string";
+	else
+	{
+		cout << "Indexes of each occurrence: ";
+		for (auto& vector : Occurrence) cout << vector << " ";
+	}
 	cout << endl;
-	delete[]Occurrence;
-
+	Occurrence = BMsearchAllRange(text, substring,17,36);
+	if (Occurrence.size() == 0) cout << "There is no such substring in the string";
+	else
+	{
+		cout << "Indexes of each occurrence in range: ";
+		for (auto& vector : Occurrence) cout << vector << " ";
+	}
+	cout << endl;
 
 	return 0;
 }
