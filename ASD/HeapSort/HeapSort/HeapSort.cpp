0a13ee3e0ef7d6983fadf23aaef92d82fd6ba5ca
Dubrovskirill <zet10set11@gmail.com>
2023-10-26 17:59:39 +0700

    Написание функций Heap-создание кучи и HeapSort-сортировка

--- ASD/HeapSort/HeapSort/HeapSort.cpp
+++ ASD/HeapSort/HeapSort/HeapSort.cpp
@@ -5,6 +5,7 @@
 #include <fstream>
 #include<vector>
 #include<iterator>
+
 void CheckSort(std::vector<int>& vec, const int& size)
 {
 	for (int i = 1; i < size; i++)
@@ -17,64 +18,80 @@ void CheckSort(std::vector<int>& vec, const int& size)
 	}
 }
 
-//bool CheckHeap(int* arr, int f, int l)
-//{
-//	int n = l - f + 1;
-//	
-//}
+void Heap(std::vector<int>& vec, const int& n,const int &ind)
+{
+	int l = ind;
+	int ch1 = 2 * ind + 1; 
+	int ch2 = 2 * ind + 2;
+	if (ch1<n && vec[ch1]>vec[l])
+		l = ch1;
+	if (ch2<n && vec[ch2]>vec[l])
+		l = ch2;
+	if (l != ind)
+	{
+		std::swap(vec[l], vec[ind]);
+		Heap(vec, n, l);
+	}
+}
+
+void HeapSort(std::vector<int>& vec, const int size)
+{
+	for (int i = size / 2 - 1; i >= 0; i--)
+		Heap(vec, size, i);
+
+	for (int n = size - 1; n >= 0; n--)
+	{
+		std::swap(vec[0], vec[n]);
+		Heap(vec, n, 0);
+	}
+
+}
+
+void Input(int j, int i, std::vector<int>& vector)
+{
+	std::string s = "arr_size_" + std::to_string(i) + "_in_range_" + std::to_string(j) + ".txt";
+	std::ifstream inputFile(s);
+	if (inputFile.is_open())
+	{
+		int num;
+		while (inputFile >> num)
+			vector.push_back(num);
+	}
+	else std::cout << "File opening error!\n";
+	inputFile.close();
+}
 
 int main()
 {
-	const int size = 9;
-	int arr[size] = { 6,42,55,12,4,32,24,5,12 };
-	int count = 1;
-	std::cout << count << ". ";
-	count++;
-	for (int i = 0; i < 9; i++)
-		std::cout << arr[i] << " ";
-	std::cout << std::endl;
-	
-	for (int n=size;n>1;n--)
+	using std::cout;
+	using std::cin;
+	using std::endl;
+    int count = 1;
+	for (int j = 10; j <= 100000; j *= 100)
 	{
-		for (int ind = n / 2 - 1; ind >= 0; ind--)
+		for (int i = 10000; i <= 1000000; i *= 10)
 		{
-			int a1 = 2 * ind + 1;
-			int	a2 = 2 * ind + 2;
-			if (a2 >= n) a2 = -1;
-			if (a2 != -1)
+			float time[3];
+			float avti = 0;
+			cout << count << ". " << endl;
+			count += 1;
+			for (int t = 0; t < 3; t++)
 			{
-				if (arr[ind] < arr[a1] || arr[ind] < arr[a2])
-				{
-					if (arr[a1] >= arr[a2]) std::swap(arr[a1], arr[ind]);
-					else std::swap(arr[a2], arr[ind]);
-				}
+				std::vector<int>vector;
+				Input(j, i, vector);
+				const int size = vector.size();
+				auto start = std::chrono::high_resolution_clock::now();
+				HeapSort(vector, size);
+				auto end = std::chrono::high_resolution_clock::now();
+				time[t] = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+				cout << "size: " << i << " range: " << j << " time: " << time[t] << " ms" << endl;
+				CheckSort(vector, size);
+				avti += time[t];
 			}
-			else
-			{
-				if (arr[ind] < arr[a1])
-				{
-					std::swap(arr[a1], arr[ind]);
-				}
-			}
-			std::cout << count << ". ";
-			count++;
-			for (int i = 0; i < 9; i++)
-				std::cout << arr[i] << " ";
-			std::cout << std::endl;
+			avti /= 3;
+			cout << "Average time: " << avti << " ms" << "\n----------------------\n";
+
 		}
-
-		std::cout << std::endl;
-		std::swap(arr[0], arr[n - 1]);
-		for (int i = 0; i < 9; i++)
-			std::cout << arr[i] << " ";
-		std::cout << std::endl << std::endl;
 	}
-	std::cout << count << ". ";
-	count++;
-	for (int i = 0; i < 9; i++)
-		std::cout << arr[i]<<" ";
-	
-
-
 	return 0;
 }
