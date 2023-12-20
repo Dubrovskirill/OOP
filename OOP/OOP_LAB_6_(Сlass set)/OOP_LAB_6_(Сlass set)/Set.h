4db4c9db595b7edc272ce2a14772c5e46de34773
Dubrovskirill <zet10set11@gmail.com>
2023-12-20 15:34:52 +0700

    Создание файла проекта

---
+++ OOP/OOP_LAB_6_(Сlass set)/OOP_LAB_6_(Сlass set)/Set.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "..\..\..\OOP\OOP_LAB_3_(Ñlass of boolean vector)\OOP_LAB_3_(Ñlass of boolean vector)\BoolVector.h"
+#include "..\..\..\OOP\OOP_LAB_4_(Ñlass of boolean matrix)\BoolMatrix\BoolMatrix.h"
+class Set:  public BoolVector
+{
+public:
+
+
+private:
+
+};
+
+
